6dd07301483018b041fb066869a0ccf9b9489d06
Saliu Fadlullahgit config --global user.name Saliu <l00179048@atu.iel00179048@atu.iegit config --global user.email l00179048@atu.ie>
2024-03-14 13:53:18 +0000

    commit

---
+++ pratical 1.R
@@ -0,0 +1,41 @@
+x <- c(3, 12,7)
+
+y <- c(4, 9, 11)
+
+z <- x+y
+
+p <- c(2)
+
+q <- c(50)
+
+x1 <- x + p
+
+y1 <- y + q
+
+sum(x1)
+
+mean(y1)
+
+w <- c(2,7,NA, 20)
+
+sum(w)
+
+v <- sum(w,na.rm = TRUE)
+v
+
+typeof(v)
+
+x1 > y1
+x > y
+
+
+
+x<-append(x,12)
+x
+x1 <- append(x1, 12)
+x1
+u<-c(5:20)
+u
+u1<-append(u,letters[6:10])
+u1
+
